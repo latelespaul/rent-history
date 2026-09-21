578abf4d3759f0a68be08348fcf1eacf92c529a1
Diwakar <the.walkersingh@hotmail.com>
2026-09-21 23:28:44 +0530

    - cors added in WebConfig.java

--- rentrix-server/src/main/java/com/rentrix/rentrixserver/config/WebConfig.java
+++ rentrix-server/src/main/java/com/rentrix/rentrixserver/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.rentrix.rentrixserver.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,13 +9,15 @@ public class WebConfig implements WebMvcConfigurer {
 	
 	// This pulls the URL you just defined in your application-prod.yaml
 	// If it's missing, it defaults to '*' (or you can specify a local fallback)
-	@Value("${app.cors.allowed-origins:*}")
-	private String allowedOrigins;
-	
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**") // Allows CORS for all endpoints in your app
-				  .allowedOrigins(allowedOrigins)
+				  .allowedOrigins(
+					  "http://localhost:5173",
+					  "https://rentrix-sage.vercel.app",
+					  "https://myrentrix.vercel.app",
+					  "http://localhost:8080/swagger-ui"
+				  )
 				  .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 				  .allowedHeaders("*")
 				  .allowCredentials(true);
