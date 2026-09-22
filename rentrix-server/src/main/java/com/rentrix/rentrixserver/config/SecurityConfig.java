597899e3b39d5ffe3d8597c395bbe57e10529287
Diwakar <the.walkersingh@hotmail.com>
2026-09-22 21:21:55 +0530

    - SecurityConfig change for cors

--- rentrix-server/src/main/java/com/rentrix/rentrixserver/config/SecurityConfig.java
+++ rentrix-server/src/main/java/com/rentrix/rentrixserver/config/SecurityConfig.java
@@ -100,10 +100,10 @@ public class SecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowedOrigins(List.of(
+		config.setAllowedOriginPatterns(List.of(
 			"http://localhost:5173",
-			"https://rentrix-sage.vercel.app",
-			"https://myrentrix.vercel.app"
+			"https://myrentrix.vercel.app",
+			"https://*.vercel.app"
 		));
 		config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
 		config.setAllowedHeaders(List.of("*"));
